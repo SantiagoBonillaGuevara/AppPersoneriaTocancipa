b095e290402209213289192d365c5a95edd1e8fe
santi <sbonilla284@gmail.com>
2025-05-26 01:03:21 -0500

    algunos warnings eliminados (calidad de codigo)

--- app/src/main/java/com/personeriatocancipa/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/personeriatocancipa/app/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.personeriatocancipa.app.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.personeriatocancipa.app.R
 import com.personeriatocancipa.app.databinding.FragmentHomeBinding
-import com.personeriatocancipa.app.ui.user.UserActivity
 
 class HomeFragment : Fragment() {
 
